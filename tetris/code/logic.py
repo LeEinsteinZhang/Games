dc8c0cc5e93d5d8ca20a9fe5d87a7557d3360eca
lez <leeinsteinzhang@gmail.com>
2023-09-05 14:11:05 -0400

    update logic module

--- tetris/code/logic.py
+++ tetris/code/logic.py
@@ -3,32 +3,55 @@ from block import *
 
 
 def check_collision(grid, shape, offset):
-    off_x, off_y = offset
-    for i, row in enumerate(shape):
-        for j, cell in enumerate(row):
+    x_shift, y_shift = offset
+    for i in range(len(shape)):
+        row = shape[i]
+        for j in range(len(row)):
+            cell = row[j]
             if cell:
-                if i + off_x > len(grid) - 1 or j + off_y > len(grid[i]) - 1 or j + off_y < 0 or grid[i + off_x][j + off_y]:
+                if (i + x_shift > len(grid) - 1) or \
+                   (j + y_shift > len(grid[i]) - 1) or \
+                   (j + y_shift < 0) or \
+                   (grid[i + x_shift][j + y_shift]):
                     return True
     return False
 
+
 def remove_line(grid, line):
     del grid[line]
-    return [[0 for _ in range(GRID_WIDTH)]] + grid
+    new_line = [[0] * GRID_WIDTH]
+    return new_line + grid
+
 
 def join_matrix(grid, shape, shape_id, offset):
-    off_x, off_y = offset
-    for i, row in enumerate(shape):
-        for j, cell in enumerate(row):
+    x_shift, y_shift = offset
+    for i in range(len(shape)):
+        row = shape[i]
+        for j in range(len(row)):
+            cell = row[j]
             if cell:
-                grid[i + off_x][j + off_y] = shape_id
+                grid[i + x_shift][j + y_shift] = shape_id
     return grid
 
+
 def new_board():
-    board = [[0 for x in range(GRID_WIDTH)] for y in range(GRID_HEIGHT)]
+    board = []
+    for i in range(GRID_HEIGHT):
+        row = []
+        for j in range(GRID_WIDTH):
+            row.append(0)
+        board.append(row)
     return board
 
+
 def clear_lines(grid):
-    lines_to_clear = [i for i, row in enumerate(grid) if all(row)]
+    lines_to_clear = []
+    for i, row in enumerate(grid):
+        if all(row):
+            lines_to_clear.append(i)
+    lines_removed_count = 0
     for i in lines_to_clear:
-        grid = remove_line(grid, i)
-    return len(lines_to_clear), grid
+        grid = remove_line(grid, i - lines_removed_count)
+        lines_removed_count += 1
+
+    return lines_removed_count, grid
